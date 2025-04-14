b84305b0071d8202444bf1ddb405d7057688178f
8bstn <sebsilva10@hotmail.com>
2025-04-13 23:29:30 -0600

    Update App.js

--- my-lms-app/src/App.js
+++ my-lms-app/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
-import CoursePage from "./CoursePage";
+import CoursesPage from "./CoursePage";
 import LoginForm from "./components/LoginForm";
+import SignupPage from "./SignupPage";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/courses" element={<CoursePage />} />
-        <Route path="/login" element={<LoginForm />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/courses" element={<CoursesPage />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
