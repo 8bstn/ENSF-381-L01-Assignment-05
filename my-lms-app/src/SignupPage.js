853483eb37a89199e824214b23362bff3225f5f6
8bstn <sebsilva10@hotmail.com>
2025-04-13 23:29:53 -0600

    Create SignupPage.js

---
+++ my-lms-app/src/SignupPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import RegForm from "./components/RegForm";
+import Footer from "./components/Footer";
+
+const SignupPage = () => (
+  <div>
+    <Header />
+    <RegForm />
+    <Footer />
+  </div>
+);
+
+export default SignupPage;
